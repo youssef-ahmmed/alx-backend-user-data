d3829e37cd9bf04500c65c5354ea12063507194e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-09 18:57:52 +0200

    Task2[Add]: Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,16 +2,33 @@
 """Regex-ing"""
 import logging
 import re
-from typing import List
+from typing import Tuple, Sequence
+
+PII_FIELDS: Tuple = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str,
+def filter_datum(fields: Sequence[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
     return re.sub(r'(\w+)=([^{}]+)'.format(separator),
                   lambda match: '{}={}'.format(match.group(1), redaction)
                   if match.group(1) in fields
                   else '{}={}'.format(match.group(1), match.group(2)), message)
+
+
+def get_logger() -> logging.Logger:
+    """Get logger with specific configs"""
+    logger = logging.getLogger("user_data")
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
@@ -22,7 +39,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: Sequence[str]):
         """Call super formatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
