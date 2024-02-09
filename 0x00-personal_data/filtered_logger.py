84c76bbec18cb37e9b34548735b2e772cca46a4d
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-09 18:23:48 +0200

    Task1[Add]: Implement log formatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Regex-ing"""
-from typing import List
+import logging
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
                   lambda match: '{}={}'.format(match.group(1), redaction)
                   if match.group(1) in fields
                   else '{}={}'.format(match.group(1), match.group(2)), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Call super formatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format log record"""
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
