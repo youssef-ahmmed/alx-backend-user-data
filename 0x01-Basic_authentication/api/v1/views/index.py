5a013baf156e0684140a1b2171fc716dc8196126
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-13 07:26:54 +0200

    Handle unauthorized and forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'],strict_slashes=False)
+def unauthorized() -> str:
+    """ Get /api/v1/unauthorized
+    Return:
+        - raising 401 status error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Get /api/v1/unauthorized
+    Return:
+        - raising 403 status error
+    """
+    abort(403)
