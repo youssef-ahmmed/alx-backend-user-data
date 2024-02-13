84c9dd087ce5e17b0022b4aad77cfc6578f98d46
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-13 15:22:30 +0200

    Implement method to get the Base64 part of auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """Implement basic auth for end points API"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic auth class"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns base64 of Authorization header"""
+        if not authorization_header or type(authorization_header) != str:
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
