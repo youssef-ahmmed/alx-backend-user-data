ef12a7f9a7e122e882a56b586c1e42ac26ea9a4c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-13 14:59:56 +0200

    Validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,8 @@
 Route module for the API
 """
 from os import getenv
+
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -11,6 +13,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -32,6 +40,27 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def handle_auth() -> None:
+    """Handle authorization before sending requests"""
+    if not auth:
+        return
+
+    excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    require_auth = auth.require_auth(request.path, excluded_list)
+    if not require_auth:
+        return
+
+    auth_header = auth.authorization_header(request)
+    if not auth_header:
+        abort(401)
+
+    curr_user = auth.current_user(request)
+    if not curr_user:
+        abort(403)
 
 
 if __name__ == "__main__":
