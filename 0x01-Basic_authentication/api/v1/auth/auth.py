d2369abe339fbff787671d4bce573c825a7ccd36
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-13 07:07:11 +0200

    Implement Auth class to manage API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class to manage API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ define paths that require auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Define the authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Check for the current user to require auth"""
+        return None
