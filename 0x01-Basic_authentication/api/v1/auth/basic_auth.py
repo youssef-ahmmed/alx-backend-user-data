569c82941e4a753c432647281739056c90a38900
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-14 07:02:29 +0200

    Implement full Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,3 +72,16 @@ class BasicAuth(Auth):
             return None
 
         return user_objs[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        base64_credentials = self.extract_base64_authorization_header(
+            auth_header)
+        decoded_credentials = self.decode_base64_authorization_header(
+            base64_credentials)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_credentials)
+        user = self.user_object_from_credentials(user_email, user_pwd)
+
+        return user
