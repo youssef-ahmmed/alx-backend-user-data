d5a4ac946c9e4d49d42523ccf15d3c037bd2dab6
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-17 13:01:46 +0200

    Create a session with id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 """Implement session auth for end points API"""
+import uuid
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session auth class"""
-    pass
+
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session id for a user"""
+        if not user_id or type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
