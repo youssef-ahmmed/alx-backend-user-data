f474de1dc98913213dd6a5247c5271c91c793c1b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-17 12:50:44 +0200

    Template implementation of Session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -20,6 +21,8 @@ auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "basic_auth":
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    auth = SessionAuth()
 elif auth_type == "auth":
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Implement session auth for end points API"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session auth class"""
+    pass
