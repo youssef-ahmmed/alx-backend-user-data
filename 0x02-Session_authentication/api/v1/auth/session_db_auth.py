cf774896ca3ed488b778767906f7a17344cf3f5c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-17 19:07:33 +0200

    Add exceptions and more checks

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,7 @@
 """Implement session auth for end points API"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -22,8 +23,23 @@ class SessionDBAuth(SessionExpAuth):
     def user_id_for_session_id(self, session_id=None):
         """ Returns the User ID by requesting UserSession
             in the database based on session_id"""
-        user_id = UserSession.search({"session_id": session_id})
-        return user_id if user_id else None
+        if not session_id:
+            return None
+
+        UserSession.load_from_file()
+        user_sessions = UserSession.search({"session_id": session_id})
+
+        if not user_sessions:
+            return None
+
+        user_session = user_sessions[0]
+        expired_time = user_session.created_at + \
+                       timedelta(seconds=self.session_duration)
+
+        if expired_time < datetime.utcnow():
+            return None
+
+        return user_session.user_id
 
     def destroy_session(self, request=None):
         """ Destroys the UserSession based on the Session ID
@@ -35,10 +51,24 @@ class SessionDBAuth(SessionExpAuth):
         if not session_id:
             return False
 
-        user_session = self.user_id_by_session_id(session_id)
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        user_session = UserSession.search({
+            'session_id': session_id
+        })
+
         if not user_session:
             return False
 
-        user_session[0].remove()
+        user_session = user_session[0]
+
+        try:
+            user_session.remove()
+            UserSession.save_to_file()
+        except Exception:
+            return False
 
         return True
