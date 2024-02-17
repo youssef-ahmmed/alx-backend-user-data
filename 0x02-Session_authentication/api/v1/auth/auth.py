6edb935d78f6528e44c7848c86f326b1a7c12b4c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-17 13:18:31 +0200

    Get the session cookie value

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ template for an authentication system"""
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -35,3 +36,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Check for the current user to require auth"""
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request"""
+        if not request:
+            return None
+        
+        return request.cookies.get(getenv("SESSION_NAME"))
