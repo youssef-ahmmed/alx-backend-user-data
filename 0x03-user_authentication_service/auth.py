d18e5ea01105d4275066636e05e4a76702d1872b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 16:26:20 +0200

    Add method to validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,12 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the login is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
