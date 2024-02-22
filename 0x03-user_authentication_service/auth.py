0b31024fd532c57860485e9dff09506074f40761
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 17:08:34 +0200

    Delete session id of existing user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Delete the session id of existing user"""
+        return self._db.update_user(user_id, session_id=None)
