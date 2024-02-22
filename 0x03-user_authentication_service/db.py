3d44b56af14c69b78a410748d94d5fd68a537446
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 13:06:19 +0200

    Disable echo mode

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -20,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -36,13 +36,9 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Create a new user"""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            user = None
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
