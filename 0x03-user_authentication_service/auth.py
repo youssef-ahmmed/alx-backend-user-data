b972588ddeacaee0cb28755a9affaf48222ff234
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 16:36:23 +0200

    Update existing user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,15 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session for existing users"""
+        try:
+            user = self._db.find_user_by(email=email)
+
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
+        except NoResultFound:
+            return None
