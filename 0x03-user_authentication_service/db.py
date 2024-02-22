153a0bbbefb86b880a9bd56a031eeb2d776b4a0f
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 12:58:53 +0200

    Query to find a user by any keywords

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """DB module
 """
+from typing import Type
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +41,11 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> Type[User]:
+        """Get user filtered by input args"""
+        filtered_user = self._session.query(User).filter_by(**kwargs).first()
+        if not filtered_user:
+            raise NoResultFound
+
+        return filtered_user
