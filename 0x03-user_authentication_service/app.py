bcf741f6e0cbeb6a43129ba6a53c271a4ebc6d0a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 17:47:07 +0200

    Implement get_reset_password_token endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,5 +86,18 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", method=["POST"])
+def get_reset_password_token():
+    """POST /reset_password"""
+    email = request.form.get("email")
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
