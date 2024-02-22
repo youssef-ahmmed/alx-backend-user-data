988c741579077d8ed13893dcb4502f1136f751bf
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 17:47:54 +0200

    Add status code to endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,10 +93,9 @@ def get_reset_password_token():
 
     try:
         reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
-
-    return jsonify({"email": email, "reset_token": reset_token})
 
 
 if __name__ == "__main__":
