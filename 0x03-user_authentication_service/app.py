61ce53f629482693b6913ae66694a0ca05480296
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-22 17:19:29 +0200

    Implement logout endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Simple Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 from auth import Auth
 
@@ -54,6 +54,23 @@ def login() -> str:
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """DELETE /sessions
+    Return:
+        - Success logout and redirect
+        or
+        - Failed logout, 403
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
