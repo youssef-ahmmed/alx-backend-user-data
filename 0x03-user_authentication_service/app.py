bac6e49daef78f5aecdb0f4ed8643dbed4477011
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-23 06:36:20 +0200

    Change route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -98,7 +98,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/update_password", methods=["PUT"])
+@app.route("/reset_password", methods=["PUT"])
 def update_password():
     """PUT /reset_password"""
     email = request.form.get("email")
